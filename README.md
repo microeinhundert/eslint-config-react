96a5b6267e4289383d04f4f6fee0628359c914b4
Leon Seipp <l.seipp@microeinhundert.com>
2021-11-09 18:38:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ESLint Config React
+
+ESLint config for react projects.
+
+## Install
+
+```
+npm install @microeinhundert/eslint-config-react --save-dev
+```
+
+## Usage
+
+Create a file named `.eslintrc.json` with following contents in the root folder of your project:
+
+```json
+{
+  "extends": "@microeinhundert/eslint-config-react"
+}
+```
+
+That's it! You can override the settings by editing the `.eslintrc.json` file.
---
+++ src/index.js
@@ -0,0 +1,140 @@
+const resolvableFileExtensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es2021: true,
+    node: true,
+    browser: true,
+  },
+  plugins: ['import', 'simple-import-sort', 'sonarjs', 'formatjs', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:import/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:tailwind/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: resolvableFileExtensions,
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // React
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandLast: true,
+        reservedFirst: true,
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+
+    // Formatjs
+    'formatjs/enforce-id': [
+      'error',
+      {
+        idInterpolationPattern: '[sha512:contenthash:base64:6]',
+      },
+    ],
+    'formatjs/enforce-placeholders': 'error',
+
+    // Import
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        ts: 'never',
+        json: 'always',
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/prefer-default-export': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    // Other
+    camelcase: 'off',
+    'no-shadow': 'off',
+    'no-bitwise': 'off',
+    'no-nested-ternary': 'off',
+    'no-empty-pattern': 'off',
+    'consistent-return': ['error', { treatUndefinedAsUnspecified: true }],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'prettier/prettier': 'error',
+    'sonarjs/no-duplicate-string': 'off',
+    'sonarjs/no-nested-template-literals': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.{test,spec}.{js,jsx,ts,tsx}'],
+      plugins: ['cypress'],
+      extends: ['plugin:cypress/recommended'],
+    },
+    {
+      files: ['*.fixture.{js,jsx,ts,tsx}'],
+      rules: {
+        'react-hooks/rules-of-hooks': 'off',
+      },
+    },
+    {
+      files: ['*.{ts,tsx}'],
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+      ],
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': resolvableFileExtensions,
+        },
+        'import/resolver': {
+          typescript: {},
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          2,
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
